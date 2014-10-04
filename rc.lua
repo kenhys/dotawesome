f9827f168e2f1dcf220eb3abe6e25958fa98de16
HAYASHI Kentaro <kenhys@gmail.com>
2014-10-04 23:24:58 +0900

    Add gnome-sound-applet

--- rc.lua
+++ rc.lua
@@ -378,4 +378,5 @@ client.add_signal("unfocus", function(c) c.border_color = beautiful.border_norma
 
 
 awful.util.spawn_with_shell("nm-applet")
+awful.util.spawn_with_shell("gnome-sound-applet")
 
